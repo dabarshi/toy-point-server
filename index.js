0ad2e87f3a370c45143fa3ca1e4a7870fd214327
dabarshi <70447223+dabarshi@users.noreply.github.com>
2023-05-23 01:19:10 +0600

    resolved sorting error

--- index.js
+++ index.js
@@ -18,116 +18,120 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    // create database
-    const toyDBCollection = client.db('toyDB').collection('toyCollection');
+        // create database
+        const toyDBCollection = client.db('toyDB').collection('toyCollection');
 
-    // simple get operation with limit and search query
-    app.get('/toys', async(req, res) => {
-        let query = {};
-        let sorting = {};
+        // simple get operation with limit and search query
+        app.get('/toys', async (req, res) => {
+            let query = {};
 
-        // set query for search by toy Name
-        if(req.query?.toyName){
-            query = {toyName: req.query.toyName}
-        }
 
-        // set query for searching subCategory
-        if (req.query?.subCategory){
-            query = {subCategory: req.query.subCategory}
-        }
-        // set query for sorting value
-        if(req.query?.sorting){
-            sorting = {price: parseInt(req.query.sorting)}
-        }
-        // console.log(query)
-        const cursor = toyDBCollection.find(query).limit(20).sort(sorting);
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-
-    // get toys with query Email 
-    app.get('/selective-toys', async(req, res) => {
-       
-        let query = {};
-        if(req.query?.email){
-            query = {email: req.query.email}
-        }
-        
-        const result = await toyDBCollection.find(query).toArray();
-        res.send(result)
-    })
-
-    // get with id
-
-    app.get('/toy/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await toyDBCollection.findOne(query);
-        res.send(result);
-    })
-
-    // post operation for add a toy info
-    app.post('/toys', async(req, res) => {
-        const toyInfo = req.body;
-        console.log('toyInfo: ', toyInfo)
-        const result = await toyDBCollection.insertOne(toyInfo);
-        res.send(result);
-    })
-
-    // put operation on single document
-    app.put('/toy/:id', async(req, res) => {
-        const id = req.params.id;
-        const updatedToyInfo = req.body;
-        const filter = {_id: new ObjectId(id)};
-        const options = {upsert: true};
-
-        const newToyInfo = {
-            $set: {
-                toyName: updatedToyInfo.toyName,
-                toyPhoto: updatedToyInfo.toyPhoto,
-                sellerName: updatedToyInfo.sellerName,
-                email: updatedToyInfo.email,
-                subCategory: updatedToyInfo.subCategory,
-                price: updatedToyInfo.price,
-                rating: updatedToyInfo.rating,
-                quantity: updatedToyInfo.quantity,
-                details: updatedToyInfo.details
+            // set query for search by toy Name
+            if (req.query?.toyName) {
+                query = { toyName: req.query.toyName }
             }
-        }
-        
-        const result = await toyDBCollection.updateOne(filter, newToyInfo, options);
-        res.send(result);
 
-    })
+            // set query for searching subCategory
+            if (req.query?.subCategory) {
+                query = { subCategory: req.query.subCategory }
+            }
 
-    // delete operation with id params 
-    app.delete('/toy/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await toyDBCollection.deleteOne(query);
-        res.send(result);
+           
+            const cursor = toyDBCollection.find(query).limit(20);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-    })
+        // get toys with query Email 
+        app.get('/selective-toys', async (req, res) => {
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+            let query = {};
+            let sorting = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+
+            // set query for sorting value
+            if (req.query?.sorting) {
+                sorting = { price: parseInt(req.query.sorting) }
+            }
+
+
+            const result = await toyDBCollection.find(query).sort(sorting).toArray();
+            res.send(result)
+        })
+
+        // get with id
+
+        app.get('/toy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toyDBCollection.findOne(query);
+            res.send(result);
+        })
+
+        // post operation for add a toy info
+        app.post('/toys', async (req, res) => {
+            const toyInfo = req.body;
+            console.log('toyInfo: ', toyInfo)
+            const result = await toyDBCollection.insertOne(toyInfo);
+            res.send(result);
+        })
+
+        // put operation on single document
+        app.put('/toy/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedToyInfo = req.body;
+            const filter = { _id: new ObjectId(id) };
+            const options = { upsert: true };
+
+            const newToyInfo = {
+                $set: {
+                    toyName: updatedToyInfo.toyName,
+                    toyPhoto: updatedToyInfo.toyPhoto,
+                    sellerName: updatedToyInfo.sellerName,
+                    email: updatedToyInfo.email,
+                    subCategory: updatedToyInfo.subCategory,
+                    price: updatedToyInfo.price,
+                    rating: updatedToyInfo.rating,
+                    quantity: updatedToyInfo.quantity,
+                    details: updatedToyInfo.details
+                }
+            }
+
+            const result = await toyDBCollection.updateOne(filter, newToyInfo, options);
+            res.send(result);
+
+        })
+
+        // delete operation with id params 
+        app.delete('/toy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toyDBCollection.deleteOne(query);
+            res.send(result);
+
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
